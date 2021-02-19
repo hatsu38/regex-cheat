3816e6082f43151f823e5156facb3e9c12db56fd
hatsu38 <hajiwata0308@gmail.com>
2021-02-19 16:51:40 +0900

    Fix Flags Modal & Form

--- components/flags-modal.tsx
+++ components/flags-modal.tsx
@@ -1,28 +1,22 @@
 import React, { useState } from 'react'
 import RegexFlagsForm from "./regex-flags-form"
 
-export default function FlagsModal() {
-  const [showModal, setShowModal] = useState(false);
-
+export default function FlagsModal(props) {
+  const { setShowModal } = props
   return (
     <React.Fragment>
-      <a className="dark:text-green-400" onClick={() => setShowModal(true)}>Flags</a>
-      {showModal &&
-        <div className="modal-wrapper">
-          <a className="modal-overlay" onClick={() => setShowModal(false)}></a>
-          <div className="modal-window">
-            <div className="modal-content">
-              <h2 className="head-title dark:text-green-400">Flags</h2>
-              <RegexFlagsForm />
-              <div className="">
-                <button type="button" name="button" className="text-white" onClick={() => setShowModal(false)}>
-                  閉じる
-              </button>
-              </div>
-            </div>
+      <div className="modal-wrapper">
+        <a className="modal-overlay" onClick={() => setShowModal(false)}></a>
+        <div className="modal-window">
+          <div className="modal-content">
+            <h2 className="head-title dark:text-green-400">Flags</h2>
+            <RegexFlagsForm />
+            <button type="button" name="button" className="text-white" onClick={() => setShowModal(false)}>
+                閉じる
+            </button>
           </div>
         </div>
-      }
+      </div>
     </React.Fragment>
   )
 }
