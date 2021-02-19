e5bff1951fb76fc11ac3453c0ac9c3ad33b127a5
hatsu38 <hajiwata0308@gmail.com>
2021-02-19 18:56:20 +0900

    フォームのGrid修正

--- components/regex-form.tsx
+++ components/regex-form.tsx
@@ -27,17 +27,17 @@ export default function RegexForm() {
         <span className="dark:text-green-400">正規表現</span>
         <a className="dark:text-green-400" onClick={() => setShowModal(true)}>Flags</a>
       </div>
-      <div className="flex justify-between text-2xl py-4 form-input mt-1 block w-full rounded-md dark:bg-gray-100">
+      <div className="grid grid-cols-9 justify-between text-2xl py-4 form-input mt-1 block w-full rounded-md dark:bg-gray-100">
         <ContentEditable
           html={`${regexText}`}
           disabled={false}
           onChange={e => updateRegexText(e.target.value)}
           tagName='div'
-          className="dark:text-gray-700"
+          className="dark:text-gray-700 col-span-8"
         />
-        <div className="dark:text-gray-500 outline-none flex" onClick={() => setShowModal(true)}>
+        <div className="dark:text-gray-500 text-right outline-none" onClick={() => setShowModal(true)}>
           <span>{trueFlagsKeys}</span>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg className="inline-block w-4 ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" />
           </svg>
         </div>
