4fb2ea1f53f54a00cd1a7a022f0b801616fef869
hatsu38 <hajiwata0308@gmail.com>
2021-03-03 16:42:24 +0900

    yarn add react-redux

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,19 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+type State = {
+  selected_name: string
+}
+
+const useText = () => {
+  const selected_name = useSelector((state: State) => state.selected_name)
+  return { selected_name }
+}
+
+
 const Header: React.FC = () => {
+  const { selected_name } = useText()
+
   return (
     <header className="bg-gray-600 fixed z-50 h-16 w-full shadow-md flex items-center justify-between grid grid-cols-12">
       <div className="flex items-center h-full col-span-2 border-r border-gray-300">
@@ -11,7 +26,7 @@ const Header: React.FC = () => {
       <div className="flex items-center text-sm col-span-10 h-full">
         <div className="flex items-center w-full text-left">
           <a href="/" className="text-white px-4 w-full">
-            タイトル
+            {selected_name}
           </a>
         </div>
         <div className="flex items-center w-full text-right">
